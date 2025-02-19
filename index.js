8a0dec1c76d9e340429f77b3ad429dd1d0f47946
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-18 21:57:52 -0300

    Exercício de Arrow Functions

---
+++ index.js
@@ -0,0 +1,5 @@
+//Exercício 1: Arrow Function com um parâmetro
+// Função arrow que recebe um número e retorna o dobro dele
+const dobro = numero => numero * 2;
+console.log(dobro(7));
+console.log(dobro(15)); 
